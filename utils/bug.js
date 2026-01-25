9e86bd1625b0d54fa87f5884caf32c033128403d
Ibrahima sory Sacko <imirisacko@gmail.com>
2026-01-25 23:51:12 +0000

    Create bug.js

---
+++ utils/bug.js
@@ -0,0 +1,24 @@
+async function bug(message, client, texts, num) {
+    try {
+        const remoteJid = message.key?.remoteJid;
+        await client.sendMessage(remoteJid, {
+            image: { url: `database/${num}.jpg` },
+            caption: `> ${texts}`,
+            contextInfo: {
+                externalAdReply: {
+                    title: "Join Our WhatsApp Channel",
+                    body: " 𓆩 IB_HEX_BOT 𓆪 ",
+                    mediaType: 1,
+                    thumbnailUrl: `https://https://chat.whatsapp.com/DSvc4vCcdZEER4DodjvhIT`,
+                    renderLargerThumbnail: false,
+                    mediaUrl: `${num}.jpg`,
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default bug;
