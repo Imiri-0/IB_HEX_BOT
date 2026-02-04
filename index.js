84c2258cdcfcff466aa51c6902a51541ad37720a
Ibrahima sory Sacko <imirisacko@gmail.com>
2026-02-04 12:07:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import connectToWhatsapp from './Digix/crew.js';
-import handleIncomingMessage from './events/messageHandler.js';
+import connectToWhatsapp from './Digix/crew.js'
+import handleIncomingMessage from './events/messageHandler.js'
 
-(async () => {
-  await connectToWhatsapp(handleIncomingMessage);
-  console.log('✅ WhatsApp connection established from index.js!');
-})();
+(async() => {
+    await connectToWhatsapp(handleIncomingMessage)
+        console.log('established !')
+})()
