d5c225aa1455fda3dc8f8f159eaefa2082a8b3f0
Ibrahima sory Sacko <imirisacko@gmail.com>
2026-02-04 12:34:04 +0000

    Update react.js

--- commands/react.js
+++ commands/react.js
@@ -1,10 +1,21 @@
-export default async function react(client, message) {
-  const remoteJid = message.key.remoteJid;
 
-  await client.sendMessage(remoteJid, {
-    react: {
-      text: '🐦‍🔥',
-      key: message.key
-    }
-  });
+export async function react(client, message) {
+
+    const remoteJid = message.key.remoteJid;
+
+   await client.sendMessage(remoteJid, 
+
+        {
+            react: {
+                text: '🐦‍🔥',
+
+                key: message.key
+            }
+        }
+
+    )
+
 }
+
+
+export default react;
